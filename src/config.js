6465a43256cceacfde0f08ad7497a635e77bdac0
vision-2 <biplovdhakal@gmail.com>
2025-10-11 11:55:52 +0545

    d

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // API configuration for WearUp
 const API_BASE = import.meta.env.DEV
   ? 'http://localhost:8000/api'
-  : 'https://wearup-django.onrender.com/api';
+  : 'https://wearup-django-1.onrender.com/api';
 
 const MEDIA_BASE = import.meta.env.DEV
   ? 'http://localhost:8000'
-  : 'https://wearup-django.onrender.com';
+  : 'https://wearup-django-1.onrender.com';
 
 export { API_BASE, MEDIA_BASE };
